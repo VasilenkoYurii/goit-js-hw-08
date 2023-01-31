6dcf846d36bd93e5098116a7ae08f280d7b56d3d
VasilenkoYurii <yura.vasilenko98@gmail.com>
2023-01-31 12:15:22 +0200

    add more

---
+++ src/js/01-gallery.js
@@ -0,0 +1,25 @@
+// Add imports above this line
+import { galleryItems } from './gallery-items';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+// Change code below this line
+
+const galleryContainer = document.querySelector('.gallery');
+const imgCards = createImgCardsMarkup(galleryItems);
+galleryContainer.insertAdjacentHTML('beforeend', imgCards);
+
+function createImgCardsMarkup(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `
+    <a class="gallery__item" href="${original}">
+        <img class="gallery__image" src="${preview}" alt="${description}" />
+    </a>`;
+    })
+    .join('');
+}
+
+let gallery = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: '250',
+});
