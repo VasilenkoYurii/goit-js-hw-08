a7233ab74c2b75ce243bdf6066bcc1765d6e0f47
VasilenkoYurii <yura.vasilenko98@gmail.com>
2023-02-01 15:35:15 +0200

    complete

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,25 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const STORAGE_KEY = 'videoplayer-current-time';
+
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+let seconds = 0;
+
+playVideo();
+
+player.on('timeupdate', throttle(time, 1000));
+
+function time(data) {
+  seconds = Math.round(data.seconds);
+  localStorage.setItem(STORAGE_KEY, seconds);
+}
+
+function playVideo() {
+  const onSecond = localStorage.getItem(STORAGE_KEY);
+
+  if (onSecond > 0) {
+    player.setCurrentTime(onSecond);
+  }
+}
